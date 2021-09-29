f21675c4573386e4f4b1a5348f0b4d0dd985e96a
Robert Bayer <robo.bayer@gmail.com>
2021-09-29 11:26:00 +0200

    spmv kernels

--- weeklies/weekly-2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
+++ weeklies/weekly-2/w2-code-handin/cuda/spMV-Mul-kernels.cu.h
@@ -3,22 +3,40 @@
 
 __global__ void
 replicate0(int tot_size, char* flags_d) {
-    // ... fill in your implementation here ...
+    const unsigned int lid = threadIdx.x;
+    const unsigned int gid = blockIdx.x*blockDim.x + lid;
+    if (gid < tot_size) {
+        flag_d[gid] = 0;
+    }
 }
 
 __global__ void
 mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
-    // ... fill in your implementation here ...
+    const unsigned int lid = threadIdx.x;
+    const unsigned int gid = blockIdx.x*blockDim.x + lid;
+    if (gid < mat_rows) {
+        flags_d[mat_shp_sc_d[gid]] = 1;
+    }
 }
 
 __global__ void 
 mult_pairs(int* mat_inds, float* mat_vals, float* vct, int tot_size, float* tmp_pairs) {
-    // ... fill in your implementation here ...
+    const unsigned int lid = threadIdx.x;
+    const unsigned int gid = blockIdx.x*blockDim.x + lid;
+    // let prods = map (\(i,x) -> x*vct[i]) mat_val
+    if (gid < tot_size) {
+        tmp_pairs[gid] = mat_vals[gid] * vct[ mat_inds[gid] ];
+    }
 }
 
 __global__ void
 select_last_in_sgm(int mat_rows, int* mat_shp_sc_d, float* tmp_scan, float* res_vct_d) {
-    // ... fill in your implementation here ...
+    const unsigned int lid = threadIdx.x;
+    const unsigned int gid = blockIdx.x*blockDim.x + lid;
+    if (gid < mat_rows) { 
+        const unsigned int id = if gid == (mat_rows-1) mat_rows-1 else mat_shp_sc_d[gid] + mat_shp_sc_d[gid+1]; 
+        res_vct_d[gid] = tmp_scan[id]
+    }
 }
 
 #endif
